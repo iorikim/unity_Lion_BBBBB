132806c2389514b1c8a96351fe05305e9de9c470
iorikim <63327355+iorikim@users.noreply.github.com>
2020-05-01 17:19:13 +0800

    lesson3 finish

--- Lesson 3 practice/Assets/bat.cs
+++ Lesson 3 practice/Assets/bat.cs
@@ -4,32 +4,51 @@ using UnityEngine.UI;
 public class bat : MonoBehaviour
 {
     /// <summary>
-    /// 蝙輻的血量
+    /// 蝙蝠的血量
     /// </summary>
-    [Header("血量"), Range(0,500)]
-    public int HP = 240;
-    
+    [Header("血量"), Range(0, 500)]
+    public int 血量 = 240;
+
     /// <summary>
-    /// 蝙輻的攻擊力
+    /// 蝙蝠的攻擊力
     /// </summary>
-    [Header("攻擊力"),Range(10,100)]
-    public int ATK = 30;
-    
+    [Header("攻擊力"), Range(10, 100)]
+    public int 攻擊力 = 30;
+
     /// <summary>
-    /// 蝙輻的治癒量
+    /// 蝙蝠的治癒量
     /// </summary>
     [Header("治癒量"), Range(10, 100)]
-    public int REC = 10;
+    public int 治癒量 = 10;
 
+    [Header("文字:輸出結果")]
+    public Text result;
+    public slime slime1;
 
+    /// <summary>
+    /// 蝙蝠攻擊
+    /// </summary>
+    public void ATK()
+    {
+        result.text = slime1.name + "承受" + 攻擊力 + "點傷害";
+        slime1.injured();
+    }
 
+    /// <summary>
+    /// 蝙蝠被攻擊
+    /// </summary>
+    public void injured()
+    {
+        int allhp = 血量 -= slime1.攻擊力;
+        result.text += "\n" + gameObject.name + "當前血量:" + allhp;
+    }
 
-
-
-    
-
-
-
-
-
+    /// <summary>
+    /// 蝙蝠回復血量
+    /// </summary>
+    public void Recovery()
+    {
+        result.text = gameObject.name + "回復" + 治癒量 + "點血量" + "\n"
+            + gameObject.name + "當前血量:" + (血量 += 治癒量);
+    }
 }
--- Lesson 3 practice/Assets/slime.cs
+++ Lesson 3 practice/Assets/slime.cs
@@ -1,4 +1,5 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 
 public class slime : MonoBehaviour
 {
@@ -6,17 +7,51 @@ public class slime : MonoBehaviour
     /// 史萊姆的血量
     /// </summary>
     [Header("血量"), Range(0, 500)]
-    public int HP = 150;
+    public int 血量 = 150;
 
     /// <summary>
     /// 史萊姆的攻擊力
     /// </summary>
     [Header("攻擊力"), Range(10, 100)]
-    public int ATK = 10;
+    public int 攻擊力 = 10;
 
     /// <summary>
     /// 史萊姆的治癒量
     /// </summary>
     [Header("治癒量"), Range(10, 100)]
-    public int REC = 10;
+    public int 治癒量 = 20;
+
+
+    [Header("文字:輸出結果")]
+    public Text result;
+    public bat bat1;
+
+    /// <summary>
+    /// 史萊姆攻擊
+    /// </summary>
+    public void ATK()
+    {
+        result.text = bat1.name + "承受" + 攻擊力 + "點傷害";
+        bat1.injured();
+    }
+
+    /// <summary>
+    /// 史萊姆被攻擊
+    /// </summary>
+    public void injured()
+    {
+        int allhp = 血量 -= bat1.攻擊力;
+        result.text += "\n" + gameObject.name + "當前血量:" + allhp;
+    }
+
+    /// <summary>
+    /// 史萊姆回復血量
+    /// </summary>
+    public void Recovery()
+    {
+        result.text = gameObject.name + "回復" + 治癒量 + "點血量" + "\n"
+            + gameObject.name + "當前血量:" + (血量 += 治癒量);
+
+
+    }
 }
